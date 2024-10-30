c54e86dfa732aef70f08bbafc95e85de5af06868
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-10-30 13:40:08 +0800

    sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Api\ApiController;
 
 Route::post('/login', [ApiController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/attendances', [ApiController::class, 'index']);
