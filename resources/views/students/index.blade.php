b5e96c9bde7d6a7cb38031c500a0ad61df51e688
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-11-07 17:21:13 +0800

    update

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -24,12 +24,12 @@
                         <x-text-input type="text" name="middlename" />
                     </div>
                     <div>
-                        <x-input-label for="student_no" :value="__('Student No')" />
+                        <x-input-label for="student_no" :value="__('RFID No')" />
                         <x-text-input type="text" name="student_no" />
                     </div>
-                    <div class="flex justify-end mt-2">
+                    {{-- <div class="flex justify-end mt-2">
                         <x-primary-button type="submit">Save</x-primary-button>
-                    </div>
+                    </div> --}}
                 </form>
             </div>
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg w-2/3">
