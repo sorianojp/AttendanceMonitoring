2141a7f4adaba2a2dc7b3dcbbfee434d1062f296
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2024-11-07 16:46:39 +0800

    update

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+          // Create an admin user
+          User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@isudd.com',
+            'password' => Hash::make('Universidad2001'),
+        ]);
+    }
+}
